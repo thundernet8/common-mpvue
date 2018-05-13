4908e71730948a10ddaeb9b9c468530425ed2506
thundernet8 <wuxueqian2010@hotmail.com>
2018-05-13 23:40:58 +0800

    v1.0.4

--- src/web/polyfill.ts
+++ src/web/polyfill.ts
@@ -2,41 +2,39 @@ import wx from './wx';
 
 declare var window;
 
-export default function polyfill() {
-    let _wx;
+let _wx;
 
+export default function polyfill() {
     // 判断当前容器环境
     const isUnderBrowser =
         typeof window === 'object' && Object.prototype.toString.call(window) === '[object Window]';
 
-    return (function() {
-        if (_wx) {
-            return _wx;
-        }
-        if (isUnderBrowser) {
-            console.warn('auto polyfill wx/getApp/getCurrentPages for mp');
-            // polyfill wx
-            window.wx = wx;
-            // polyfill getApp
-            const getApp = function() {
-                return window.__mpapp__;
-            };
-            window.getApp = getApp;
-            // polyfill getCurrentPages
-            const getCurrentPages = function() {
-                return [];
-            };
-            window.getCurrentPages = getCurrentPages;
-
-            _wx = {
-                wx,
-                getApp,
-                getCurrentPages
-            };
-        } else {
-            _wx = {};
-        }
-
+    if (_wx) {
         return _wx;
-    })();
+    }
+    if (isUnderBrowser) {
+        console.warn('auto polyfill wx/getApp/getCurrentPages for mp');
+        // polyfill wx
+        window.wx = wx;
+        // polyfill getApp
+        const getApp = function() {
+            return window.__mpapp__;
+        };
+        window.getApp = getApp;
+        // polyfill getCurrentPages
+        const getCurrentPages = function() {
+            return [];
+        };
+        window.getCurrentPages = getCurrentPages;
+
+        _wx = {
+            wx,
+            getApp,
+            getCurrentPages
+        };
+    } else {
+        _wx = {};
+    }
+
+    return _wx;
 }
